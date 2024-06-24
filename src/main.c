6ee07bbfd01cec773d93a6d272e05f51cd199098
danbsh <58045839+danbsh@users.noreply.github.com>
2024-06-24 22:18:50 +0100

    add state, create editor window

--- src/main.c
+++ src/main.c
@@ -6,19 +6,28 @@
 
 #include "nown.h"
 
-// TODO: vault path should be configurable
+static app_state *state;
 
 int main(void) {
-  init_main_screen();
-  vault files = load_vault();
-  print_menu(files);
+  state = (app_state *)malloc(sizeof(app_state));
+  init_app();
 
   while (getch() != KEY_F(1)) {
   }
 
+  free(state->vault);
+  free(state->menu);
+  free(state);
   endwin();
-  free(files.note_files);
+
   return 0;
+}
+
+void init_app() {
+  init_main_screen();
+  load_vault();
+  init_menu();
+  init_editor();
 }
 
 void init_main_screen() {
@@ -26,29 +35,62 @@ void init_main_screen() {
   raw();
   noecho();
   keypad(stdscr, TRUE);
+  refresh();
 }
 
-vault load_vault() {
+void init_menu(note_vault files) {
+  int menu_len = COLS / MENU_W;
+
+  state->menu = (note_menu *)malloc(sizeof(note_menu));
+  state->menu->win = newwin(LINES, menu_len, 0, 0);
+  state->menu->highlight = 2;
+  state->menu->choice = 2;
+
+  print_menu();
+}
+
+void init_editor() {
+  int menu_len = COLS / MENU_W;
+  int editor_len = COLS - menu_len;
+
+  state->editor = (note_editor *)malloc(sizeof(note_editor));
+  state->editor->win = newwin(LINES, editor_len, 0, menu_len);
+  box(state->editor->win, 0, 0);
+  wrefresh(state->editor->win);
+}
+
+void load_vault() {
   struct dirent **namelist;
   int n;
+
+  state->vault = (note_vault *)malloc(sizeof(note_vault));
 
   if ((n = scandir("./vault", &namelist, NULL, alphasort)) == -1) {
     perror("Error scanning vault.");
     exit(EXIT_FAILURE);
   }
 
-  vault files = {namelist, n};
-
-  return files;
+  state->vault->files = namelist;
+  state->vault->n = n;
 }
 
-void print_menu(vault files) {
-  for (int i = 0; i < files.n; i++) {
-    char *name = files.note_files[i]->d_name;
+void print_menu() {
+  for (int i = 0; i < state->vault->n; i++) {
+    char *name = state->vault->files[i]->d_name;
+
+    if (i == 1) {
+      wattron(state->menu->win, A_BOLD);
+      mvwprintw(state->menu->win, i, 1, "Notes");
+      wattroff(state->menu->win, A_BOLD);
+    }
 
     if (strcmp(name, ".") == 0 || strcmp(name, "..") == 0)
       continue;
 
-    printw("%s\n", files.note_files[i]->d_name);
+    mvwprintw(state->menu->win, i, 1, "%s\n", state->vault->files[i]->d_name);
   }
+
+  box(state->menu->win, 0, 0);
+  wmove(state->menu->win, state->menu->highlight, 1);
+  wrefresh(state->menu->win);
 }
--- src/nown.h
+++ src/nown.h
@@ -1,8 +1,32 @@
-typedef struct _note_vault {
-  struct dirent **note_files;
-  int n;
-} vault;
+#include <ncurses.h>
 
+typedef struct _note_vault {
+  struct dirent **files;
+  int n;
+} note_vault;
+
+typedef struct _note_menu {
+  WINDOW *win;
+  int highlight;
+  int choice;
+} note_menu;
+
+typedef struct _note_editor {
+  WINDOW *win;
+} note_editor;
+
+typedef struct _app_state {
+  note_vault *vault;
+  note_menu *menu;
+  note_editor *editor;
+  WINDOW *active_win;
+} app_state;
+
+#define MENU_W 3
+
+void init_app();
 void init_main_screen();
-vault load_vault();
-void print_menu(vault files);
+void init_menu();
+void init_editor();
+void load_vault();
+void print_menu();
